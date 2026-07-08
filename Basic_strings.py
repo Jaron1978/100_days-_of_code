4831bc148813a681f2313c228053945b4acb95bb
Ron Jackson <jacksonr1978@gmail.com>
2026-07-08 13:28:59 +0100

    Updated Basic_strings.py

--- Basic_strings.py
+++ Basic_strings.py
@@ -1,2 +1,7 @@
 # Writing out basic print functions.
-print("Hello World")
+print('Hello World')
+print('Day 1 - Python print fuctions and the use of stings.')
+
+# A function is declared inside a set of double quotes
+
+print(print('What to print'))
