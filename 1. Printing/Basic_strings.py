603aaa608d24e6b578c7037534406a2e68861112
Ron Jackson <jacksonr1978@gmail.com>
2026-07-09 13:37:47 +0100

    Added folder structure

--- 1. Printing/Basic_strings.py
+++ 1. Printing/Basic_strings.py
@@ -1,6 +1,6 @@
 # Writing out basic print functions.
 print('Hello World')
-print('Day 1 - Python print fuctions and the use of stings.')
+print('1. Printing - Python print fuctions and the use of stings.')
 
 # A function is declared inside a set of double quotes
 
