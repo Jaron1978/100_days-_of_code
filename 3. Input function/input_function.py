1d5529285d7ae2aba5bfa83055589b3b6385339e
Ron Jackson <jacksonr1978@gmail.com>
2026-07-09 14:31:54 +0100

    Completed the len test.

--- 3. Input function/input_function.py
+++ 3. Input function/input_function.py
@@ -1,1 +1,5 @@
+# Added in the input function. Prints Hello, then prompts the user to enter their name.
+
+# On Windows, start at the end of a line and Ctrl + / to make that line a comment.
+
 print('Hello ' +  input('What is your name?'))
---
+++ 4. Input Excercise/Input_excercise.py
@@ -0,0 +1,3 @@
+# Use functions to establish how many characters are in a name.
+
+print( len( input('What is your name? ') ) )
