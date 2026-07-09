a9418e7a29f413262022f94a390f506f1561f156
Ron Jackson <jacksonr1978@gmail.com>
2026-07-09 14:11:24 +0100

    Using the input function and minor a spelling error for Day 1 Code

--- 1. Printing/Basic_strings.py
+++ 1. Printing/Basic_strings.py
@@ -1,6 +1,6 @@
 # Writing out basic print functions.
 print('Hello World')
-print('1. Printing - Python print fuctions and the use of stings.')
+print('1. Printing - Python print functions and the use of stings.')
 
 # A function is declared inside a set of double quotes
 
