40d1b2bb9172bbceb1aa88e39129c9cbb7f285ba
Ron Jackson <jacksonr1978@gmail.com>
2026-07-09 13:47:32 +0100

    Fixed the code for this task. I have chose to use single colon rather than double colon.

--- 2. Debugging/day-1-2 excercise - Fix_the_code.py
+++ 2. Debugging/day-1-2 excercise - Fix_the_code.py
@@ -1,2 +1,17 @@
 # Fix the code below
 
+""" Errored Code"""
+#
+# print(Day 1 - String Manipulation)
+# print("String concatenation is done with the "+" sign".)
+    # print('e.g print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n".)
+
+""" Fixed Code """
+print('Day 1 - String Manipulation')
+print('String manipulation is done with the " + " sign')
+print('e.g. print("Hello " + " "world"")')
+print('New lines can be created with a backslash and n.')
+
+
+
