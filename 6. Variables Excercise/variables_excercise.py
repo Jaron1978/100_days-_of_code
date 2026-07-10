97de5a2113252311ce62f8528f4b31f9fa0c7117
Ron Jackson <jacksonr1978@gmail.com>
2026-07-10 13:20:19 +0100

    Variables lesson completed.

---
+++ 6. Variables Excercise/variables_excercise.py
@@ -0,0 +1,14 @@
+a = input("a:")
+b = input("b:")
+
+##################################
+# Write code below this line.
+c = a
+a = b
+b = c
+
+
+# Write code above this line.
+###################################
+print("a = " + a)
+print("b = " + b)
