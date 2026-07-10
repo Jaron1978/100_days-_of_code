8471144ca4c137f794b687d109c2fb228943074a
Ron Jackson <jacksonr1978@gmail.com>
2026-07-10 14:19:50 +0100

    Day 1 Final Project completed.

---
+++ 8. Day 1 Final Project/day_1_final_project.py
@@ -0,0 +1,13 @@
+# Create a greeting for your program
+print('Hi there, Welcome to the band name generator. Lets get started!')
+
+# Ask the user for thew city they grew up in.
+city = input('Where did you grow up? \n')
+
+# Ask the user for the name of their pet
+pet = input('What was the name of your pet? \n')
+
+# Combine the name of the city and their pet for the band name
+print('Your band name is ' + city + ' ' + pet)
+
+# Make sure the input cursor shows on a new line.
